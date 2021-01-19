a5ce93f94b1cf86de3865efb6c9b162c88975a91
martinmen <martinhmsuni@gmail.com>
2021-01-19 17:23:00 -0300

    Ejercicio de State

--- fitness/src/index.js
+++ fitness/src/index.js
@@ -10,4 +10,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Card/>, container)
+ReactDOM.render(<Card
+                    title="Mi titulo"
+                    description="Mi descripcion"
+                    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                    leftColor="#A74CF2"
+                    rightColor="#617BFB"
+                />, container)
